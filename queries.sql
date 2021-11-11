6984b19c1959f0c56f905002d3eba0d711ea37de
Jessica Zager <jmzager@gmail.com>
2021-11-11 17:32:28 -0600

    Assignment 4, Task 3 & Hopefully 4 almost done the sequel

--- queries.sql
+++ queries.sql
@@ -8,5 +8,8 @@ FROM employer
 WHERE (location="St. Louis City");
 
 ## Part 3: Test it with SQL
+--Write a SQL statement to remove the job table.
 
 ## Part 4: Test it with SQL
+--Write a query to return a list of the names & descriptions of all skills that are attached to jobs in alphabetical order.
+--With making use of "is not null", include a condition that won't list a skill if does not have a job listed.'
